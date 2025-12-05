635c7eee7ae54b1986c8fd3f29b43b31dca632c4
321voiture321 <108331451+321voiture321@users.noreply.github.com>
2025-12-05 04:05:40 +0100

    Delete javascript/randommoney.js

--- javascript/randommoney.js
+++
@@ -1,6 +0,0 @@
-//recup argent
-let argent = JSON.parse(localStorage.getItem("argent") || '{"monnaie":1000}');
-// modif argent
-argent.monnaie += 100;
-// uplod argent
-localStorage.setItem("argent", JSON.stringify(argent));
